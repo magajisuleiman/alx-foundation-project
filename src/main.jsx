5c4fcd88a0a5876f4137e5588b189b99575bc34f
magajisuleiman <magajisuleiman1203@gmail.com>
2023-12-15 22:35:41 +0100

    updated profile page and menu page

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Profile from './Profile.jsx'
+import Navbar from './NavBar.jsx'
+import Footer from './Footer.jsx'
+import Menu from './Menu.jsx'
+import MenuCard from './MenuCard.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Navbar />
+    <Menu />
+    <MenuCard />
+    <Footer />
   </React.StrictMode>
 )
